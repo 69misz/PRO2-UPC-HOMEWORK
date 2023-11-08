7dc44f47ec0f188cfa813dd1b3d3e3e5649b1466
69misz <132626893+69misz@users.noreply.github.com>
2023-11-07 21:04:57 -0400

    video2

---
+++ CodigoFacilito/program.cs
@@ -0,0 +1,21 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Presentacion1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            // Este en un programa de Hola mundo
+            /* Este en un cometario
+             de bloque*/
+             Console.WriteLine("Hola mundo");
+             Console.ReadKey();
+             
+        }
+    }
+}
