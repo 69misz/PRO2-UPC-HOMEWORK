7e17cd59f1532c461b1e9dcf752cdc6ac8ed03b7
69misz <132626893+69misz@users.noreply.github.com>
2023-11-07 21:25:56 -0400

    video4

---
+++ Programacion3.cs
@@ -0,0 +1,29 @@
+using System;
+using System.Collections.Generic;
+using System.Ling;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Presentacion3
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //variable
+            int i = 10;
+            decimal x = 12.2m;
+            bool bandera = true;
+            string cadena = "12344";
+            DateTime fecha =DateTime.MinValue;
+            i = Convert.ToInt32(cadena);
+            Console.WriteLine("El valor i es: {0}", i);
+            Console.WriteLine("El valor x es: {0:C}", x);
+            Console.WriteLine("El valor de bandera es: " + bandera.ToString());
+            Console.WriteLine("El valor de cadena: " + cadena);
+            Console.WriteLine("El valor de fecha: " + fecha.ToShortDateString());
+            Console.ReadKey();
+
+        }
+    }
+}
