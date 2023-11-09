faba2afefeb761f991c50bace963d3e89ed562de
69misz <sotozanabria9@gmail.com>
2023-11-08 21:45:19 -0400

    tarea1

---
+++ Tarea1 en clases/Program.cs
@@ -0,0 +1,17 @@
+﻿namespace tarea1
+{
+    class program
+    {
+    static void Main(string[] args)
+    {
+        Console.WriteLine("Area de un Cuadrado");
+        int resp = calcularAreaCuadrado(10);
+        Console.WriteLine(resp);
+    }
+    static int calcularAreaCuadrado(int L){
+        int resultado = L*L;
+        return resultado;
+    }
+    }
+}
+
